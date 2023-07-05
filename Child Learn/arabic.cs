e891a7eac70aa65e310b0214ff7bd214ee68d05c
Mahmoud Ibrahim <130683686+Mahmoud-ibrahim74@users.noreply.github.com>
2023-07-05 16:15:55 +0300

    arabic.cs

---
+++ Child Learn/arabic.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Child_Learn
+{
+    public partial class arabic : Form
+    {
+        public arabic()
+        {
+            InitializeComponent();
+            f_letter.Hide();
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+        }
+
+
+        private void bunifuImageButton2_Click_3(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                   
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            player.SoundLocation = Environment.CurrentDirectory + @"\sound\2.wav";
+            player.Play(); 
+        }
+
+        private void bunifuCheckbox1_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("تشغيــــل الصوت التعليمي", bunifuCheckbox1);
+        }
+
+        private void bunifuCheckbox1_OnChange(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            if(bunifuCheckbox1.Checked)
+            {
+                player.SoundLocation = Environment.CurrentDirectory + @"\sound\1.wav";
+                player.Play(); 
+            }
+            else
+            {
+                player.Stop();
+            }
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+
+            if(string.IsNullOrEmpty(f_letter.Text))
+            {
+                MessageBox.Show(" (^____^)  اكتب حرف الالــف اولا للأنتـــقال الي الدرس التالي", "خطــأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if(f_letter.Text != "أ")
+            {
+                MessageBox.Show(" (^____^)  اكتب حرف الالــف صحيــح مثل (أ) للأنتـــقال الي الدرس التالي", "خطــأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+            {
+                player.SoundLocation = Environment.CurrentDirectory + @"\sound\happy.wav";
+                player.Play(); 
+                MessageBox.Show(" (^____^)    احسنــــت هيا بنا الي الدرس التالي", "تـــم", MessageBoxButtons.OK, MessageBoxIcon.None);
+                new baa().Show();
+                this.Hide();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(f_letter.Visible == false)
+            {
+                animator1.ShowSync(f_letter);
+
+            }
+
+            
+        }
+
+        private void bunifuImageButton1_MouseEnter(object sender, EventArgs e)
+        {
+            toolTip1.Show("الانتــــقال الي الدرس التالي", bunifuImageButton1);
+
+        }
+
+        private void bunifuImageButton1_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.RemoveAll();
+        }
+    }
+}
